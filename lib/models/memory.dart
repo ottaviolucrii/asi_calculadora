c78d0b39c12444d8ea9dc1b77cc57e18661ab09f
Ottavio Lucri De Souza <ottaviolucrinot@gmail.com>
2025-02-15 18:21:52 -0300

    terminando de implemantar logica no applycommand, criando medoto calculate, logica do all clear terminada

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,35 +1,43 @@
 class Memory {
-
   static const operations = ['%', '/', 'x', '-', '+', '='];
-
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  late String operation;
+  late String _operation;
   String _value = '0';
   bool _whipeValue = false;
+  String _lastCommand = '';
 
-
-  String get value{
+  String get value {
     return _value;
   }
 
-
   void applyCommand(String command) {
-    if(command  == 'AC') {
-      _allClear();
-    } else if(operations.contains(command)) {
-      _setOperation(command);
+    if (_isReplacingOperation(command)) {
+      _operation = command;
+      return;
     }
-    else {
+    if (command == 'AC') {
+      _allClear();
+    } else if (operations.contains(command)) {
+      _setOperation(command);
+    } else {
       _addDigit(command);
     }
+    _lastCommand = command;
   }
 
-  _addDigit (String digit){
+  _isReplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        operations.contains(command) &&
+        _lastCommand != '=' &&
+        command != '=';
+  }
+
+  _addDigit(String digit) {
     final isDot = digit == '.';
     final wipeValue = (_value == '0' && !isDot) || _whipeValue;
-    if(isDot && _value.contains('.') && !wipeValue) {
+    if (isDot && _value.contains('.') && !wipeValue) {
       return;
     }
     final emptyValue = isDot ? '0' : '';
@@ -40,34 +48,48 @@ class Memory {
     _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
-  _calculate () {
-    switch(operation){
-      case '%': return _buffer[0] % _buffer[1];
-      case '/': return _buffer[0] / _buffer[1];
-      case 'x': return _buffer[0] * _buffer[1];
-      case '-': return _buffer[0] - _buffer[1];
-      case '+': return _buffer[0] + _buffer[1];
-      default: return _buffer[0];
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
     }
   }
 
-  _setOperation(String newOperation){
-    if(_bufferIndex == 0){
-      operation = newOperation;
-      _bufferIndex = 1;
-      _whipeValue = true;
+  _setOperation(String newOperation) {
+      bool isEqualSign = newOperation == '=';
+    if (_bufferIndex == 0) {
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _whipeValue = true;
+      }
     } else {
       _buffer[0] = _calculate();
       _buffer[1] = 0.0;
 
       _value = _buffer[0].toString();
       _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
-      operation = newOperation;
-      _bufferIndex = 1;
+      _operation = isEqualSign ? _operation : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
+      _whipeValue = !isEqualSign;
     }
   }
 
   void _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _bufferIndex = 0;
+    _operation = '';
+    _whipeValue = false;
   }
 }
